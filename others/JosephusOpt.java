3e5920a3ab37bb3567b454282cb3456381c91602
BenWent <1914351357@qq.com>
2020-08-02 19:46:37 +0800

    optmize Josephus using mathmatics

--- others/JosephusOpt.java
+++ others/JosephusOpt.java
@@ -4,11 +4,13 @@
 public class JosephusOpt{
     public static void main(String[] args){
         // 205769
-        int n = Integer.MAX_VALUE / 10000;
-        int q = Integer.MAX_VALUE / 1000000;
+        int n = 7;
+        int q = 3;
 
         if (n == 1) {
             System.out.println(1);
+
+            return;
         }
 
         // 最后一个报数的人的编号满足递推公式 f[n] = (f[n - 1] + q) % n; // 编号从 0 开始
@@ -20,12 +22,10 @@ public class JosephusOpt{
 //        System.out.println(f[n] + 1);
 
         // 空间复杂度为 O(1) 的实现
-        int res = 0, pre = 0;
-        int index = 2;
-        while (n-- > 1) {
-            res = (pre + q) % index++;
-            pre = res;
+        int last = 0;
+        for(int i = 2; i <= n; i++) {
+            last = (last + q) % i;
         }
-        System.out.println(res + 1); // 205769
+        System.out.println(last); // 205769
     }
 }
